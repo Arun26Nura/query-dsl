10ee1a28f0d2bd3db090ffc48bd021178844f82f
Arun26Nura <arunkumarmohan1@gmail.com>
2024-02-08 15:35:24 +0530

    Joins

--- src/main/java/com/learn/jpa/controller/TestController.java
+++ src/main/java/com/learn/jpa/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.learn.jpa.controller;
 
-import com.learn.jpa.repo.EngineRepository;
+import com.learn.jpa.repo.EngineRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +11,11 @@ import java.util.List;
 public class TestController {
 
     @Autowired
-    EngineRepository engineRepo;
+    EngineRepositoryImpl engineRepo;
 
     @GetMapping("hi")
     public List<String> getEngine(){
+        System.out.println(engineRepo.findById(1));
         return engineRepo.findByType("petrol","2010");
     }
 }
--- src/main/java/com/learn/jpa/repo/EngineRepositoryImpl.java
+++ src/main/java/com/learn/jpa/repo/EngineRepositoryImpl.java
@@ -5,22 +5,27 @@ import com.learn.jpa.entity.QEngine;
 import com.learn.jpa.entity.QMotorcycle;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
-public class EngineRepository {
+@Repository
+public class EngineRepositoryImpl extends SimpleJpaRepository<Engine,Integer>  {
     @Autowired
     JPAQueryFactory jpaQueryFactory;
 
     QEngine engine= QEngine.engine;
     QMotorcycle motorcycle= QMotorcycle.motorcycle;
 
-    public Engine findById(int id){
-        return jpaQueryFactory.selectFrom(engine).where(engine.Id.eq(id)).fetchFirst();
+    public EngineRepositoryImpl( EntityManager entityManager) {
+        super(Engine.class, entityManager);
     }
+
 
     public List<String> findByType(String type, String modelName){
         return  jpaQueryFactory
